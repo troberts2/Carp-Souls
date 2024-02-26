5999815a93e7b9376b84854cb71d3d29139c12d7
Graperial <gengelmann@mail.bradley.edu>
2024-02-26 11:19:16 -0600

    Spin reworked, bullet fixed, boss look towards, and most of all boss director!
    The boss now follows orders.

---
+++ Assets/Scripts/HydroBullet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HydroBullet : MonoBehaviour
+{
+    private Rigidbody rb;
+    private GameObject player;
+
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player"); //temporary dw
+        transform.LookAt(player.transform, Vector3.down);
+        transform.Rotate(0, 90, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= (transform.right * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Destroy(transform.parent.gameObject);
+            Destroy(gameObject);
+            //also will hurt the player
+            //pop sfx
+        }
+    }
+}
