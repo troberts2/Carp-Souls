190d4ae6a9596f7222cbcc95d61ca765bf3159cf
Smash-Sonic <smashsonic21@gmail.com>
2024-02-26 19:49:12 -0600

    No more cursor while fishing

--- Assets/Scripts/QTESystem.cs
+++ Assets/Scripts/QTESystem.cs
@@ -42,7 +42,8 @@ public class QTESystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
