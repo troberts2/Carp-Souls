f0a5d5dad59b06b856551f09e45d58ed3d6ca0c2
Smash-Sonic <smashsonic21@gmail.com>
2024-02-29 16:30:13 -0600

    Menus are now controller friendly

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MenuFunctions : MonoBehaviour
 {
+    [SerializeField] private GameObject _mainMenuFirst;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        EventSystem.current.SetSelectedGameObject(_mainMenuFirst);
     }
 
     public void SceneChange(int sceneID)//sets up scene changing
     {
+        EventSystem.current.SetSelectedGameObject(null);
         SceneManager.LoadScene(sceneID);
     }
 
     public static void QuitGame()
     {
-
+        EventSystem.current.SetSelectedGameObject(null);
         Application.Quit();
         /*if (Application.isEditor)
         {
